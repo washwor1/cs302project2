cc658cd6551803b784d216c93b6cb5a561e508d8
unknown <washwor1@vols.utk.edu>
2021-09-14 22:40:48 -0400

    made list destructor

--- list.cpp
+++ list.cpp
@@ -1,3 +1,4 @@
+
 #include "volsort.h"
 
 
@@ -6,6 +7,14 @@ List::List() {
     size = 0;
 }
 
+//i wasnt exactly sure about the destructor for the linked list so i used code from stack overflow
+//https://stackoverflow.com/questions/2265967/writing-a-linkedlist-destructor
 List::~List() {
-    
+    Node* current = head;
+    while( current != 0 ) {
+        Node* next = current->next;
+        delete current;
+        current = next;
+    }
+head = 0;
 }
