e3f76f46bac391695cf1de09b5a40c7712eaf49f
Andy <azeng2@vols.utk.edu>
2021-09-16 11:45:13 -0400

    quick bandage

--- quick.cpp
+++ quick.cpp
@@ -35,7 +35,7 @@ void partition(Node *head, Node *pivot, Node *&left, Node *&right, bool numeric)
     Node *currRight,*currLeft;
     if (numeric) {
         while(curr!=nullptr){
-            std::cout << curr->number << std::endl;
+            std::cout << pivot->number << std::endl;
             if(curr->number>=pivot->number) {
                 if (right == nullptr) {
                     right = curr;
