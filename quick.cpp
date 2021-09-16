eb03c03cadd33e8ac0a2e1f9f4a7b58e9756105f
Andy <azeng2@vols.utk.edu>
2021-09-16 15:08:05 -0400

    quick

--- quick.cpp
+++ quick.cpp
@@ -26,7 +26,7 @@ Node *qsort(Node *head, bool numeric) {
     head->next=nullptr;
     left = concatenate(left,head);
     left = qsort(left,numeric);
-    std::cout << left->number << std::endl;
+    std::cout << left->number << " " << left->next << std::endl;
     right = qsort(right,numeric);
     return concatenate(left,right);
 }
